1e9f97b5e697c478186267253fb0d88b65a6f20c
Patrick Watters <pwatter1@binghamton.edu>
2017-09-11 02:39:32 +0000

    finished HW 1 pair programming assn

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.3'
+ruby '2.33'
+
 
 gem 'rspec'
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,58 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  
+  return sum(arr.sort.last(2))
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.combination(2).find{|x,y| x+y==n}
+    return true
+  else
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  (s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0 
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    # check for argument errors with input
+    raise ArgumentError if isbn.empty? || price <= 0 
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string 
+    format("$%.2f", @price)
+  end
 end
